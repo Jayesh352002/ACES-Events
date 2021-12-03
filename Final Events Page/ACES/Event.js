6226434fc3f72753888d039baf24bfc660bcabcc
Jayesh Bafna <53260266+Jayesh352002@users.noreply.github.com>
2021-12-02 19:45:03 -0800

    Add files via upload

---
+++ Final Events Page/ACES/Event.js
@@ -0,0 +1,12 @@
+function tech(){
+  window.location="Technical.html";
+}
+function sport(){
+  window.location="Sports.html";
+}
+function cul(){
+  window.location="Cultural.html";
+}
+function pub(){
+  window.location="Publicity.html";
+}
---
+++ Final Events Page/ACES/Technical.js
@@ -0,0 +1,4 @@
+function main()
+{
+    window.location="index.html"
+}
